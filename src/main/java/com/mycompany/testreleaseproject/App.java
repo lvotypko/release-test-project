2d887c593bedf368e3bd95df58f106671ce4ac39
lvotypko <lvotypko@redhat.com>
2016-05-25 14:18:51 +0200

    init

---
+++ src/main/java/com/mycompany/testreleaseproject/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.testreleaseproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
